fc5c04d1aba7108dc8bb3410ea18e0352c51fd12
farhatrv <abbasfarhat47@gmail.com>
2020-02-14 16:22:59 +0530

    adding 2 cheatsheet projects for react with react hooks and react router

---
+++ react-with-react-hooks-starting-project/src/components/Ingredients/Ingredients.js
@@ -0,0 +1,74 @@
+import React, { useState, useEffect, useCallback } from "react";
+
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
+
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  // not needed as search component is anyways making a fetch call on mounting
+  // useEffect(() => {
+  //   fetch("https://test-kdu-react-hooks.firebaseio.com/ingredients.json")
+  //     .then(response => response.json())
+  //     .then(responseData => {
+  //       const loadedIngredients = [];
+  //       for (const key in responseData) {
+  //         loadedIngredients.push({
+  //           id: key,
+  //           title: responseData[key].title,
+  //           amount: responseData[key].amount
+  //         });
+  //       }
+  //       setUserIngredients(loadedIngredients);
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    console.log("RENDERING INGREDIENTS", userIngredients);
+  }, [userIngredients]);
+
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    setUserIngredients(filteredIngredients);
+    // userIngredients.filter(enter===ing.title)
+  }, []);
+
+  const addIngredientHandler = ingredient => {
+    fetch("https://react-hooks-update.firebaseio.com/ingredients.json", {
+      method: "POST",
+      body: JSON.stringify(ingredient),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(responseData => {
+        setUserIngredients(prevIngredients => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient }
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = ingredientId => {
+    setUserIngredients(prevIngredients =>
+      prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+    );
+  };
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search onLoadIngredients={filteredIngredientsHandler} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
+      </section>
+    </div>
+  );
+};
+
+export default Ingredients;
